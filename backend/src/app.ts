d5b8a1ad4737729492a23b9ca924596c113fefa1
Izz Abdallah <x3zoabdullah@gmail.com>
2026-04-23 02:24:52 +0400

    Frontend and Backend setup

---
+++ backend/src/app.ts
@@ -0,0 +1,17 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+
+app.use(cors());
+
+
+app.get("/",(req,res) =>{
+    res.send("Hello World")
+})
+
+const PORT = 3000;
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on http://localhost: ${PORT}`)
+})
